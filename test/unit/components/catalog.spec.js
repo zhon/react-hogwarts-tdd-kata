58e35ff7d32153437f50727313bd76083e9ba725
zhon <zhon@xputah.org>
2015-06-17 07:45:00 -0600

    1.0.0.

--- test/unit/components/catalog.spec.js
+++ test/unit/components/catalog.spec.js
@@ -8,4 +8,15 @@ const TestUtils = React.addons.TestUtils;
 
 describe('Catalog component', () => {
 
+  it('renders html headers', () => {
+    const catalog = TestUtils.renderIntoDocument(
+      <Catalog />
+    );
+    const titles = TestUtils.scryRenderedDOMComponentsWithTag(catalog, 'th');
+    expect(titles[0].getDOMNode().textContent).to.equal('Class');
+    expect(titles[1].getDOMNode().textContent).to.equal('Professor');
+    expect(titles[2].getDOMNode().textContent).to.equal('Credits');
+    expect(titles[3].getDOMNode().textContent).to.equal('Time');
+  });
+
 });
