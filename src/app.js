048178389a08d2c47714f10e21c3d34b8f7c8173
zhon <zhon@xputah.org>
2016-05-03 14:45:10 -0600

    Routing now works

--- src/app.js
+++ src/app.js
@@ -1,55 +1,56 @@
 import React from 'react';
-import Router from 'react-router';
-import ReactBootstrap from 'react-bootstrap';
-import ReactRouterBootstrap from 'react-router-bootstrap';
+import ReactDOM from 'react-dom';
 
 import CatalogPage from './components/catalog-page';
 import Schedule from './components/schedule';
 import Sorting from './components/sorting';
 
-const Nav = ReactBootstrap.Nav;
-const NavItemLink = ReactRouterBootstrap.NavItemLink;
+import { Link, Router, Route, IndexRoute, hashHistory } from "react-router";
 
-const { Route, RouteHandler, DefaultRoute } = Router;
-
+const app = document.getElementById('app');
 
 class App extends React.Component {
 
   render() {
     return (
       <div style={{'padding': '10px'}}>
-        <div className="navbar navbar-inverse">
+        <nav className="navbar navbar-inverse">
           <div className="navbar-header navbar-brand" style={{'color': 'white'}}>
             Hogwarts
           </div>
-          <Nav className="nav navbar-nav">
-            <NavItemLink to="sorting" >
+          <ul className="nav navbar-nav">
+            <li>
+            <Link to="/sorting">
               sorting
-            </NavItemLink>
-            <NavItemLink to="catalog" >
+            </Link>
+            </li>
+            <li>
+            <Link to="/catalog">
               catalog
-            </NavItemLink>
-            <NavItemLink to="schedule" >
+            </Link>
+            </li>
+            <li>
+            <Link to="/schedule">
               schedule
-            </NavItemLink>
-          </Nav>
-        </div>
-        <RouteHandler />
+            </Link>
+            </li>
+          </ul>
+        </nav>
+        {this.props.children}
       </div>
     );
   }
 
 }
 
-const routes = (
-  <Route handler={App}>
-    <Route name="sorting" handler={Sorting}/>
-    <Route name="catalog" handler={CatalogPage}/>
-    <Route name="schedule" handler={Schedule}/>
-    <DefaultRoute handler={Sorting}/>
-  </Route>
-);
-
-Router.run(routes, (Handler) => {
-  React.render(<Handler/>, document.getElementById('app'));
-});
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Sorting} />
+      <Route path="sorting"  component={Sorting}/>
+      <Route path="catalog"  component={CatalogPage}/>
+      <Route path="schedule" component={Schedule}/>
+      <IndexRoute component={Sorting}/>
+    </Route>
+  </Router>,
+app);
